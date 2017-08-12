e5faf1bb6e7e43b40308843835c12885cd8ce740
yvanwangl <1012305328@qq.com>
2017-08-12 23:34:22 +0800

    add falsk

---
+++ flask_web/mvc_app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET'])
+def login_form():
+    return render_template('form.html')
+
+
+@app.route('login', methods=['POST'])
+def login():
+    form = request.form
+    username = form['username']
+    password = form['password']
+    if username == 'admin' and password == '123456':
+        return render_template('login_ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
+
+if __name__ == '__main__':
+    app.run()
